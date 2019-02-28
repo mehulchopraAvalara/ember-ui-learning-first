a1672e7430fc033341837e5dc6b2ae6e85ecedef
Mehul Chopra <Mehul.Chopra@avalara.com>
2019-02-28 16:17:44 +0530

    first learning session

---
+++ app/components/person-name.js
@@ -0,0 +1,35 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    const { first, last } = this.getProperties('first', 'last');
+    this.setProperties({
+      firstProp: first ? first : '',
+      lastProp: last ? last : '',
+    });
+  },
+
+  fullProp: computed('firstProp','lastProp', function() {
+    const { firstProp, lastProp } = this.getProperties('firstProp', 'lastProp');
+    return `${firstProp} ${lastProp}`;
+  }),
+
+  click() {
+    this.setProperties({
+      firstProp: '',
+      lastProp: '',
+    });
+  },
+
+  /* actions: {
+    onClear() {
+      this.setProperties({
+        firstProp: '',
+        lastProp: '',
+      });
+    }
+  } */
+});
---
+++ tests/integration/components/person-name-test.js
@@ -0,0 +1,49 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, pauseTest, find, fillIn, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | person-name', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders without any data', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{person-name}}`);
+    assert.equal(find('.first-name').value, '', 'first name is blank');
+    assert.equal(find('.last-name').value, '', 'last name is blank');
+    assert.equal(find('.full-name').innerText, '', 'full name is blank');
+  });
+
+  test('it renders with data', async function(assert) {
+    await render(hbs`{{person-name
+      first='mehul'
+      last='chopra'
+    }}`);
+
+    assert.equal(find('.first-name').value, 'mehul', 'first name is not blank');
+    assert.equal(find('.last-name').value, 'chopra', 'last name is not blank');
+    assert.equal(find('.full-name').innerText, 'mehul chopra', 'full name is built');
+  });
+
+  test('it builds full name appropriately', async function (assert) {
+    await render(hbs`{{person-name}}`);
+    await fillIn('.first-name', 'tony');
+    await fillIn('.last-name', 'stark');
+    
+    assert.equal(find('.full-name').innerText, 'tony stark', 'full name is built');
+  });
+
+  test('clear button functionality', async function (assert) {
+    await render(hbs`{{person-name}}`);
+    await fillIn('.first-name', 'tony');
+    await fillIn('.last-name', 'stark');
+
+    await click('.clear-btn');
+
+    assert.equal(find('.first-name').value, '', 'first name is blank');
+    assert.equal(find('.last-name').value, '', 'last name is blank');
+    assert.equal(find('.full-name').innerText, '', 'full name is blank');
+  });
+});
